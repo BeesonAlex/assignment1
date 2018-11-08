f17a2f10777813fc65e23775fb4a6a0f6f09dc3c
Alex Beeson <beeson.alexander@gmail.com>
2018-11-08 12:23:06 -0500

    2. Completed Assignment

---
+++ assignment-1.js
@@ -0,0 +1,24 @@
+var numbers = new Array();
+
+for (var i=0; i < 10; i++){
+    numbers.push('1');
+    numbers.push('2');
+    numbers.push('3');
+    numbers.push('4');
+    numbers.push('5');
+    numbers.push('5');
+    numbers.push('6');
+    numbers.push('7');
+    numbers.push('8');
+    numbers.push('9');
+}
+
+console.log(numbers[0]);
+console.log(numbers[9]);
+
+
+var car = {
+colour: ''
+}
+
+car.colour = 'black';
