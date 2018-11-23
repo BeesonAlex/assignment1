9a5a622dcc93a5630586fdc31e53eaabe9ab6438
Alex Beeson <beeson.alexander@gmail.com>
2018-11-22 22:59:25 -0500

    Resubmission Initializing

---
+++ assignment-1-resubmission.js
@@ -0,0 +1,17 @@
+var numbers = new Array();
+
+for (var i=0; i < 10; i++){
+    numbers.push(0 + i);
+}
+
+console.log(numbers[0]);
+console.log(numbers[9]);
+
+
+var car = {
+colour: ''
+}
+
+car.colour = 'black';
+
+console.log(car.colour);
